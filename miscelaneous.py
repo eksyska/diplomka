28bf976fb40b3c3a8d258602d851754340267286
eksyska <eli.korcova@gmail.com>
2026-03-29 15:35:02 +0200

    added function for writing results into csv file

--- miscelaneous.py
+++ miscelaneous.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import os
 
 def export_matrix(matrix):
     """Exports matrix to a .txt file
@@ -13,3 +15,22 @@ def export_matrix(matrix):
             f.write(formatted_row + "\n")
 
     print("Matrix saved to matrix_output.txt")
+
+def csv_results(csv_path, L, N, J, U, gamma, dissipation, M, evals, z):
+    """Append sector statistics to a CSV file, creating it if it doesn't exist."""
+
+    row = {
+        "L": L,
+        "N": N,
+        "J": J,
+        "U": U,
+        "gamma": gamma,
+        "dissipation": dissipation,
+        "M": M,
+        "#evals": len(evals),
+        "r_mean": round(np.abs(z).mean(), 4)
+    }
+
+    df = pd.DataFrame([row])
+    write_header = not os.path.exists(csv_path)
+    df.to_csv(csv_path, mode="a", header=write_header, index=False)
